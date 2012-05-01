40bcaedc91505886faa485899fb56861fbe7d2fa
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/SoapPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/SoapPlugin/build.pl
+++ lib/Foswiki/Plugins/SoapPlugin/build.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl -w
-BEGIN { 
-  unshift @INC, split(/:/, $ENV{FOSWIKI_LIBS}); 
+BEGIN {
+    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
 }
 
 use Foswiki::Contrib::Build;
 
 $build = new Foswiki::Contrib::Build('SoapPlugin');
-$build->build($build->{target});
+$build->build( $build->{target} );
 
